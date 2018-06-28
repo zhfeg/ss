96b12cfe7e880071cb423aa437028892f0f1cc13
username <847738436@qq.com>
2018-06-28 22:44:37 +0800

    first

---
+++ memorandum/src/codes/Calculator.java
@@ -0,0 +1,157 @@
+﻿
+package codes;
+
+
+
+//import java.awt.List;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Calculator 
+{
+	  public int  add(int x,int y) throws Exception {
+		  int sum= this.ad(x);
+		  return sum;
+		 // return 1;
+	 }
+	  
+	  private int ad(int x) throws Exception 
+	  {
+		return 1;
+	}
+	  
+	  public  Calculator()
+	  {
+			
+			number = new ArrayList<Double>();
+			calculation = new ArrayList<Character>();
+		  
+	  }
+	  
+
+		// 成员变量
+		private List<Double> number;
+		private List<Character> calculation;
+		private String text;
+
+		public void s()
+		{
+			
+		}
+
+		public String calculate(String text) throws Exception {
+		
+
+			init();
+		  if (getString(text)) {
+				return sum() + "";
+			} else {
+				return "error";
+			}
+			//throw  new Exception("除数不能为0");
+		}
+			
+		
+	
+		
+
+		private double sum() {
+	
+			for (int i = 0; i < calculation.size(); i++) {
+				char c = calculation.get(i);
+				switch (c) {
+				case '*':
+					number.set(i, number.get(i) * number.get(i + 1));
+					number.remove(i + 1);
+					calculation.remove(i);
+					i -= 1;
+					break;
+				case '/':
+					number.set(i, (Double) number.get(i) / number.get(i + 1));
+					number.remove(i + 1);
+					calculation.remove(i);
+					i -= 1;
+					break;
+				}
+			}
+	
+			for (int i = 0; i < calculation.size(); i++) {
+				char c = calculation.get(i);
+				switch (c) {
+				case '+':
+					number.set(i, number.get(i) + number.get(i + 1));
+					number.remove(i + 1);
+					calculation.remove(i);
+					i -= 1;
+					break;
+				case '-':
+					number.set(i, (Double) number.get(i) - number.get(i + 1));
+					number.remove(i + 1);
+					calculation.remove(i);
+					i -= 1;
+					break;
+				}
+			}
+	
+			return number.get(0);
+		}
+	
+		public void init() {
+			number.clear();// 清空数组
+			calculation.clear();
+		}
+	
+		public boolean getString(String text) {
+			this.text = text;
+			if (check()) {
+				setValue();
+				return true;
+			}
+			return false;
+		}
+
+		private void setValue() {
+			Pattern compile = Pattern.compile("\\d+");
+			Matcher matcher = compile.matcher(text);
+			while (matcher.find()) {
+				number.add(Double.parseDouble(matcher.group()));
+			}
+			Pattern compile1 = Pattern.compile("[+-/*]");
+			Matcher matcher1 = compile1.matcher(text);
+			while (matcher1.find()) {
+				calculation.add(matcher1.group().charAt(0));
+			}
+		}
+
+		private boolean check() {
+			if (text == null || !text.matches("[0-9+\\-*/]+"))// 正则表达式
+				return false;
+
+			String regex = "\\d+";// 数字
+			String regex1 = "[+\\-*/]+";// 计算符
+			if (findCount(text, regex1, 1) == -1)
+				return false;
+			return findCount(text, regex1, 1) == findCount(text, regex, 0) - 1;
+
+		}
+
+		private int findCount(String text, String regex, int len) {
+			int count = 0;
+			Pattern compile = Pattern.compile(regex);
+			Matcher matcher = compile.matcher(text);
+
+			while (matcher.find()) {
+				count++;
+				if (len > 0) {
+
+					if (matcher.group().length() != len) {
+						return -1;
+					}
+
+				}
+			}
+			return count;
+		}
+
+}
---
+++ memorandum/test/test/CalculatorTest.java
@@ -0,0 +1,46 @@
+﻿package test;
+
+
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.*;
+
+import codes.Calculator;
+
+@RunWith(PowerMockRunner.class)
+public class CalculatorTest {
+	@Test
+	@PrepareForTest(Calculator.class)
+	
+	public void testAdd() throws Exception
+	{
+		Calculator calculator =PowerMockito.mock( Calculator.class);
+		
+		when(calculator,"ad",2).thenReturn(2);
+		
+		when(calculator,"add",2,2).thenCallRealMethod();
+		assertEquals("error",2, calculator.add(2,2));
+	
+	}
+	
+	
+	@Test
+	@PrepareForTest(Calculator.class)
+	
+	public void testC() throws Exception
+	{
+		//Calculator calculator =PowerMockito.mock( Calculator.class);
+		Calculator calculator=new Calculator();
+		//when(calculator,"ad",2).thenReturn(2);
+		
+	//	when(calculator,"add",2,2).thenCallRealMethod();
+		assertEquals("error","4.0", calculator.calculate("2+2"));
+	
+	}
+	
+}
